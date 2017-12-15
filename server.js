b3481adb74ae24386e4a723f8ad0fd6be46618a5
Dimitris Loukas <dimlucas@outlook.com>
2017-12-15 16:58:15 +0200

    Configure Heroku deployment

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const distFolder = `${__dirname}/dist`;
 
-app.use(express.static(`${__dirname}/dist/index.html`));
+app.use(express.static(`${distFolder}`));
+
+app.get('/', (req, res) => {
+    res.sendFile(`${distFolder}/index.html`);
+})
 
 app.listen(PORT, () => {
     console.log(`Deploy server listening on port ${PORT}`);
