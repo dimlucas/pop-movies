eecbfb6f8df0b2ed90a03afafec9f9f003ea0679
Dimitris Loukas <dimlucas@outlook.com>
2017-12-17 14:09:40 +0200

    Register tMDb interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MoviesListComponent } from 'app/components/movies-list/movies-list.component';
@@ -8,19 +8,28 @@ import { MoviesSearchComponent } from 'app/components/movies-search/movies-searc
 import { MovieDetailsComponent } from 'app/components/movie-details/movie-details.component';
 import { MoviesService } from 'app/services/movies.service';
 import { ErrorHandlerService } from 'app/services/error-handler.service';
+import { TmdbInterceptor } from 'app/interceptors/tmdb-interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MoviesListComponent,
-    MoviesSearchComponent,
-    MovieDetailsComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [MoviesService, ErrorHandlerService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MoviesListComponent,
+        MoviesSearchComponent,
+        MovieDetailsComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule
+    ],
+    providers: [
+        MoviesService,
+        ErrorHandlerService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: TmdbInterceptor,
+            multi: true
+        }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
